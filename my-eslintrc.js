19ed2e5fa0b2c8db15dc3e673c49342a2c7b5552
m1c4lv <128991223+m1c4lv@users.noreply.github.com>
2023-06-22 13:45:02 +0200

    Update my-eslintrc.js

--- my-eslintrc.js
+++ my-eslintrc.js
@@ -1,17 +1,18 @@
 module.exports = {
-    env: {
-      commonjs: true,
-      es2021: true,
-      node: true,
-      'jest/globals': true
-    },
-    plugins: ['jest'],
-    extends: [
-      'standard'
-    ],
-    parserOptions: {
-      ecmaVersion: 12
-    },
-    rules: {
-    }
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true
+  },
+  plugins: ['jest'],
+  extends: [
+    'standard',
+    'plugin:jest/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
   }
+}
