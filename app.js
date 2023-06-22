7744bd2519fcc9bcc16913aa1cf05d2de267303d
m1c4lv <128991223+m1c4lv@users.noreply.github.com>
2023-06-22 14:03:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const http = require('http');
+const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola mundo!\n');
-});
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hola mundo!\n')
+})
 
-const port = 3000;
+const port = 3000
 server.listen(port, () => {
-  console.log(`Servidor en funcionamiento en http://localhost:${port}/`);
-});
+  console.log(`Servidor en funcionamiento en http://localhost:${port}/`)
+})
